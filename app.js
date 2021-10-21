d111626294eb603a5c06e8a2e1b581bf363f309f
cooper-sloan <cooper@isloan.net>
2021-10-21 15:13:52 -0700

    node

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ app.post('/add', function(req,res){
 
 
 });
-server.listen(3000,function(){
-    console.log("Server listening on port: 3000");
+const port = process.env.PORT || 3000
+server.listen(port,function(){
+    console.log("Server listening on port: port");
 })
 
 var ethers = require('ethers');
